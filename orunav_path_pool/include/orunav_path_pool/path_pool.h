975bce784e57e4a686f8a953219ed52700b1df73
Henrik Andreasson <henrik.andreasson@oru.se>
2020-02-05 08:34:11 +0100

    Single path search function added.

--- orunav_path_pool/include/orunav_path_pool/path_pool.h
+++ orunav_path_pool/include/orunav_path_pool/path_pool.h
@@ -46,7 +46,34 @@ public:
       }
     return false;
   }
+
+//! WARNING, this function only looks at a single path at the time - use the ASTAR search if needed.
+  orunav_generic::Paths findPaths(const orunav_generic::Pose2d &start, const orunav_generic::Pose2d &goal, double maxDistOffset, double maxAngleOffset ) const
+  {
+    orunav_generic::Paths ret;
+    for (unsigned int i = 0; i < paths_.size(); i++)
+      {
+	const orunav_generic::Pose2d &p_start = paths_[i].getPose2d(0);
+	if (orunav_generic::getDistBetween(start, p_start) > maxDistOffset)
+	  continue;
+	if (fabs(angles::normalize_angle(start(2) - p_start(2))) > maxAngleOffset)
+	  continue;
+
+	const orunav_generic::Pose2d &p_goal = paths_[i].getPose2d(paths_[i].sizePath()-1);
+    
+	if (orunav_generic::getDistBetween(goal, p_goal) > maxDistOffset)
+	  continue;
+	if (fabs(angles::normalize_angle(goal(2) - p_goal(2))) > maxAngleOffset)
+	  continue;
+	
+	  // Found a path.
+	  ret.push_back(paths_[i]);
+	    }
+    return ret;
+  }
   
+  
+
   const std::vector<orunav_generic::Path> &getPaths() const { return paths_; }
 
 private:
