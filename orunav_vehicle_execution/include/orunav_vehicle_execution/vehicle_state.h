6e90de4d59bc189b39b89613c86217898cdc5c28
Citi2 <citi2@shitty2.com>
2021-06-28 11:28:16 +0200

    Remove support legs stuff to prevent robot from not moving when forks are low

--- orunav_vehicle_execution/include/orunav_vehicle_execution/vehicle_state.h
+++ orunav_vehicle_execution/include/orunav_vehicle_execution/vehicle_state.h
@@ -81,12 +81,12 @@ public:
       ROS_ERROR("ACTIVATE_SUPPORT_LEGS not allowed as start operation...");
       return false;
     }
-    if (startOperation_ == NO_OPERATION) {
+    /*if (startOperation_ == NO_OPERATION) {
       if (forkState_ == FORK_POSITION_SUPPORT_LEGS) {
         ROS_ERROR("FORK_POSITION_SUPPORT_LEGS not allowed fork state to start operation");
         return false;
       }
-    }
+      }*/
     return true;
   }
 
@@ -128,10 +128,10 @@ public:
       //      controllerState_ = WAITING;
       ROS_INFO("SET STATE TO DRIVING!!!!");
       state_ = DRIVING;
-      if (forkState_ == FORK_POSITION_SUPPORT_LEGS) {
+      /*if (forkState_ == FORK_POSITION_SUPPORT_LEGS) {
         // Cannot start driving if the support legs is active.
         state_ = TASK_FAILED;
-      }
+	}*/
       return;
     }  
 
